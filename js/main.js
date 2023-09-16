b872dea29f14dd0cfc96d2b748e883501730f295
amija004 <amija004@gmail.com>
2023-09-16 03:22:48 -0400

    WIP Box in a box with game title text.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // main.js
+import * as PIXI from 'pixi.js';
+import startGameBox from './startGameBox';
 // Create the application helper and add its render target to the page
 let app = new PIXI.Application({
     width: 1920, height: 1080,
@@ -9,14 +11,5 @@ app.renderer.backgroundColor = 0x131313;
 app.renderer.resize(window.innerWidth, window.innerHeight);
 app.renderer.view.style.position = 'absolute';
 
+app.stage.addChild(startGameBox);
 document.body.appendChild(app.view);
-
-const Graphics = PIXI.Graphics;
-
-const rectangle = new Graphics();
-rectangle.beginFill(0xAA33BB)
-    .lineStyle(4, 0xAE3EBE, 1)
-    .drawRect(200, 200, 100, 100)
-    .endFill();
-
-app.stage.addChild(rectangle);
---
+++ js/startGameBox.js
@@ -0,0 +1,70 @@
+import * as PIXI from 'pixi.js';
+
+// Parent Container
+const startGameBox = new PIXI.Container();
+startGameBox.sortableChildren = true;
+
+
+// Child Container
+const startGameText = new PIXI.Container();
+// Send to front
+startGameText.zIndex = 100;
+
+startGameBox.addChild(startGameText);
+
+// Start screen text
+const basicText = new PIXI.Text('Sundered Starlight');
+
+basicText.x = 350;
+basicText.y = 375;
+
+startGameText.addChild(basicText);
+
+const style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 72,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#000000'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 250,
+    dropShadow: true,
+    dropShadowColor: '#FFFFFF',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 60,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: 'round',
+});
+
+// Start screen Background
+PIXI.Assets.load('media/purpleBrickWall.jpg').then((texture) =>
+{
+    const plane = new PIXI.SimplePlane(texture, 10, 10);
+
+    plane.x = 250 // app.screen.width/2;
+    plane.y = 250 // app.screen.height/2;
+    plane.zIndex = 0;
+
+    startGameBox.addChild(plane);
+
+    // Get the buffer for vertice positions.
+    const buffer = plane.geometry.getBuffer('aVertexPosition');
+
+    // Listen for animate update
+    let timer = 0;
+
+    app.ticker.add(() =>
+    {
+        // Randomize the vertice positions a bit to create movement.
+        for (let i = 0; i < buffer.data.length; i++)
+        {
+            buffer.data[i] += Math.sin((timer / 10) + i) * 0.01;
+        }
+        buffer.update();
+        timer++;
+    });
+});
+
+export default startGameBox;
